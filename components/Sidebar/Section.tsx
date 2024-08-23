e66c785d12464a82d624438acf0dab8a24798bd0
Leander Karp <leander.karp@tutanota.com>
2024-08-23 21:06:42 +0200

    Fix and refactor Section.tsx
    Solve error revolving around missing or empty children.

--- components/Sidebar/Section.tsx
+++ components/Sidebar/Section.tsx
@@ -1,5 +1,5 @@
 import { Box, Flex, IconButton } from '@chakra-ui/react';
-import React, { ReactChild, useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { VscCircleFilled, VscCircle } from 'react-icons/vsc';
 import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons';
 import { NoteContext } from '../../util/NoteContext';
@@ -9,88 +9,87 @@ export interface SectionProps {
   className: string;
 }
 
-export const Section = (props: SectionProps) => {
-  const {
-    children,
-    className, // outline
-  } = props;
-  const [open, setOpen] = useState(true);
+type SectionButtonProps = {
+  label: string;
+  className: 'viewerHeadingButton' | 'outlineHeadingButton';
+  icon: any;
+  onClick: (e: React.MouseEvent) => void;
+};
+
+const SectionButton = ({
+  label,
+  onClick,
+  className,
+  icon,
+}: SectionButtonProps) => (
+  <IconButton
+    className={className}
+    _focus={{}}
+    _active={{}}
+    aria-label={label}
+    //mr={1}
+    size="xs"
+    variant="subtle"
+    icon={icon}
+    onClick={onClick}
+    height={2}
+    width={2}
+  />
+);
+
+export const Section = ({ children, className }: SectionProps) => {
+  const [isOpen, setIsOpen] = useState(true);
   const { collapse } = useContext(NoteContext);
   useEffect(() => {
-    setOpen(!collapse);
+    setIsOpen(!collapse);
   }, [collapse]);
 
   if (className === 'h0Wrapper headingWrapper') {
     return <Box className="preHeadingContent"> {children}</Box>;
   }
-  const kids = children as ReactChild[];
+  const [head, ...rest] = Array.isArray(children) ? children : [children];
+
   return (
     <Box className={'sec'}>
       <Box display="block">
         <Flex className="headingFlex" alignItems="baseline">
-          {open && kids.length > 0 ? (
+          {isOpen ? (
             <>
-              <IconButton
-                className="viewerHeadingButton"
-                _focus={{}}
-                _active={{}}
-                aria-label="Expand heading"
-                //mr={1}
-                size="xs"
-                variant="subtle"
-                icon={<ChevronUpIcon />}
-                onClick={() => setOpen(!open)}
-                height={2}
-                width={2}
+              <SectionButton
+                label={'Collapse heading'}
+                className={'viewerHeadingButton'}
+                icon={<ChevronDownIcon />}
+                onClick={() => setIsOpen(!isOpen)}
               />
-              <IconButton
-                className="outlineHeadingButton"
-                _focus={{}}
-                _active={{}}
-                aria-label="Expand heading"
-                //mr={1}
-                size="xs"
-                variant="subtle"
-                icon={<VscCircle />}
-                onClick={() => setOpen(!open)}
-                height={2}
-                width={2}
+              <SectionButton
+                label={'Collapse heading'}
+                className={'outlineHeadingButton'}
+                icon={<VscCircleFilled />}
+                onClick={() => setIsOpen(!isOpen)}
               />
             </>
           ) : (
             <>
-              <IconButton
-                className="viewerHeadingButton"
-                _active={{}}
-                _focus={{}}
-                aria-label="Collapse heading"
-                //mr={1}
-                height={2}
-                width={2}
-                size="xs"
-                variant="subtle"
-                icon={<ChevronDownIcon />}
-                onClick={() => setOpen(!open)}
+              <SectionButton
+                label={'Expand heading'}
+                className={'viewerHeadingButton'}
+                icon={<ChevronUpIcon />}
+                onClick={() => setIsOpen(!isOpen)}
               />
-              <IconButton
-                className="outlineHeadingButton"
-                _active={{}}
-                _focus={{}}
-                aria-label="Collapse heading"
-                //mr={1}
-                height={2}
-                width={2}
-                size="xs"
-                variant="subtle"
-                icon={<VscCircleFilled />}
-                onClick={() => setOpen(!open)}
+              <SectionButton
+                label={'Expand heading'}
+                className={'outlineHeadingButton'}
+                icon={<VscCircle />}
+                onClick={() => setIsOpen(!isOpen)}
               />
             </>
           )}
-          {kids[0]}
+          {head}
         </Flex>
       </Box>
-      {open && <Box className="sectionContent">{kids.slice(1)}</Box>}
+      {isOpen && rest.length > 0 && (
+        <Box className="sectionContent">{rest}</Box>
+      )}
     </Box>
   );
 };
