2779da37e7f7f1f69c368163add743f861aba8dc
lkarp <69141474+lkarp-744@users.noreply.github.com>
2025-04-07 19:36:09 +0200

    Simplify _app.tsx
    - remove round-trip to local storage
    - Merge SubApp and App components
    - extract auxiliary functions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,211 +5,193 @@ import {
   extendTheme,
   withDefaultColorScheme,
 } from '@chakra-ui/react';
-import { useEffect, useState, useMemo, useContext } from 'react';
+import { useState, useMemo } from 'react';
 import * as d3int from 'd3-interpolate';
 
-import { ThemeContext } from '../util/themecontext';
-import { themes } from '../components/themes';
+import {
+  ThemeContext,
+  ThemeMap,
+  Theme,
+  initialTheme,
+  initialHighlightColor,
+} from '../util/themecontext';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  type Theme = [string, { [color: string]: string }];
-  const initialTheme: Theme = ['one-vibrant', themes['one-vibrant']];
-  const [isInitialized, setIsInitialized] = useState(false);
+const getBorderColor = (
+  highlightColor: string,
+  themeColors: ThemeMap
+): undefined | string => {
+  if (highlightColor === 'purple.500') {
+    return `${themeColors['violet']}aa`;
+  }
+  if (highlightColor === 'pink.500') {
+    return `${themeColors['magenta']}aa`;
+  }
+  if (highlightColor === 'blue.500') {
+    return `${themeColors['blue']}aa`;
+  }
+  if (highlightColor === 'cyan.500') {
+    return `${themeColors['cyan']}aa`;
+  }
+  if (highlightColor === 'green.500') {
+    return `${themeColors['green']}aa`;
+  }
+  if (highlightColor === 'yellow.500') {
+    return `${themeColors['yellow']}aa`;
+  }
+  if (highlightColor === 'orange.500') {
+    return `${themeColors['orange']}aa`;
+  }
+  if (highlightColor === 'red.500') {
+    return `${themeColors['red']}aa`;
+  }
 
-  const [emacsTheme, setEmacsTheme] = useState<Theme>(initialTheme);
-  const [highlightColor, setHighlightColor] = useState('purple.500');
+  return undefined;
+};
 
-  useEffect(() => {
-    if (isInitialized) {
-      localStorage.setItem('colorTheme', JSON.stringify(emacsTheme));
-    }
-  }, [emacsTheme]);
+const constructTheme = (highlightColor: string, emacsTheme: Theme) => {
+  const themeColors: ThemeMap = emacsTheme[1] as ThemeMap;
 
-  useEffect(() => {
-    if (isInitialized) {
-      localStorage.setItem('highlightColor', JSON.stringify(highlightColor));
-    }
-  }, [highlightColor]);
-
-  useEffect(() => {
-    setEmacsTheme(
-      JSON.parse(
-        localStorage.getItem('colorTheme') ?? JSON.stringify(initialTheme)
-      ) ?? initialTheme
-    );
-    setHighlightColor(
-      JSON.parse(
-        localStorage.getItem('highlightColor') ?? JSON.stringify(highlightColor)
-      ) ?? highlightColor
-    );
-    setIsInitialized(true);
-  }, []);
-
-  const themeObject = {
-    emacsTheme: emacsTheme,
-    setEmacsTheme: setEmacsTheme,
-    highlightColor: highlightColor,
-    setHighlightColor: setHighlightColor,
-  };
-  return (
-    <ThemeContext.Provider value={themeObject as typeof themeObject}>
-      <SubApp>
-        <Component {...pageProps} />
-      </SubApp>
-    </ThemeContext.Provider>
-  );
-}
-
-function SubApp(props: any) {
-  const { children } = props;
-  const { highlightColor, emacsTheme } = useContext(ThemeContext);
-  type Theme = { [color: string]: string };
-  const themeColors: Theme = emacsTheme[1] as Theme;
-  // yeah it's annoying, should put this someplace more sensible
-  const getBorderColor = () => {
-    if (highlightColor === 'purple.500') {
-      return `${themeColors['violet']}aa`;
-    }
-    if (highlightColor === 'pink.500') {
-      return `${themeColors['magenta']}aa`;
-    }
-    if (highlightColor === 'blue.500') {
-      return `${themeColors['blue']}aa`;
-    }
-    if (highlightColor === 'cyan.500') {
-      return `${themeColors['cyan']}aa`;
-    }
-    if (highlightColor === 'green.500') {
-      return `${themeColors['green']}aa`;
-    }
-    if (highlightColor === 'yellow.500') {
-      return `${themeColors['yellow']}aa`;
-    }
-    if (highlightColor === 'orange.500') {
-      return `${themeColors['orange']}aa`;
-    }
-    if (highlightColor === 'red.500') {
-      return `${themeColors['red']}aa`;
-    }
-  };
+  const borderColor = getBorderColor(highlightColor, themeColors);
   const missingColor = d3int.interpolate(
     themeColors['base1'],
     themeColors['base2']
   )(0.2);
-  const borderColor = getBorderColor();
-  const theme = useMemo(() => {
-    return {
-      colors: {
-        white: themeColors['bg'],
-        black: themeColors['fg'],
-        gray: {
-          100: themeColors['base1'],
-          200: missingColor,
-          300: themeColors['base2'],
-          400: themeColors['base3'],
-          500: themeColors['base4'],
-          600: themeColors['base5'],
-          700: themeColors['base6'],
-          800: themeColors['base7'],
-          900: themeColors['base8'],
-        },
-        blue: {
-          500: themeColors['blue'],
-        },
-        teal: {
-          500: themeColors['blue'],
-        },
-        yellow: {
-          500: themeColors['yellow'],
-        },
-        orange: {
-          500: themeColors['orange'],
-        },
-        red: {
-          500: themeColors['red'],
-        },
-        green: {
-          500: themeColors['green'],
-        },
-        purple: {
-          500: themeColors['violet'],
-        },
-        pink: {
-          500: themeColors['magenta'],
-        },
-        cyan: {
-          500: themeColors['cyan'],
-        },
-        alt: {
-          100: themeColors['bg-alt'],
-          900: themeColors['fg-alt'],
-        },
+
+  return {
+    colors: {
+      white: themeColors['bg'],
+      black: themeColors['fg'],
+      gray: {
+        100: themeColors['base1'],
+        200: missingColor,
+        300: themeColors['base2'],
+        400: themeColors['base3'],
+        500: themeColors['base4'],
+        600: themeColors['base5'],
+        700: themeColors['base6'],
+        800: themeColors['base7'],
+        900: themeColors['base8'],
       },
-      shadows: {
-        outline: '0 0 0 3px ' + borderColor,
+      blue: {
+        500: themeColors['blue'],
       },
-      components: {
-        Button: {
-          variants: {
-            outline: {
-              border: '2px solid',
+      teal: {
+        500: themeColors['blue'],
+      },
+      yellow: {
+        500: themeColors['yellow'],
+      },
+      orange: {
+        500: themeColors['orange'],
+      },
+      red: {
+        500: themeColors['red'],
+      },
+      green: {
+        500: themeColors['green'],
+      },
+      purple: {
+        500: themeColors['violet'],
+      },
+      pink: {
+        500: themeColors['magenta'],
+      },
+      cyan: {
+        500: themeColors['cyan'],
+      },
+      alt: {
+        100: themeColors['bg-alt'],
+        900: themeColors['fg-alt'],
+      },
+    },
+    shadows: {
+      outline: '0 0 0 3px ' + borderColor,
+    },
+    components: {
+      Button: {
+        variants: {
+          outline: {
+            border: '2px solid',
+            borderColor: highlightColor,
+            color: highlightColor,
+          },
+          ghost: {
+            color: highlightColor,
+            _hover: {
+              bg: `inherit`,
+              border: '1px solid',
               borderColor: highlightColor,
-              color: highlightColor,
             },
-            ghost: {
-              color: highlightColor,
-              _hover: {
-                bg: `inherit`,
-                border: '1px solid',
-                borderColor: highlightColor,
-              },
-              _active: { color: `inherit`, bg: highlightColor },
-            },
-            subtle: {
-              color: 'gray.800',
-              _hover: { bg: `inherit`, color: highlightColor },
-              _active: { color: `inherit`, bg: borderColor },
-            },
+            _active: { color: `inherit`, bg: highlightColor },
           },
-        },
-        Accordion: {
-          baseStyle: {
-            container: {
-              marginTop: '10px',
-              borderWidth: '0px',
-              _last: {
-                borderWidth: '0px',
-              },
-            },
-            panel: {
-              marginRight: '10px',
-            },
+          subtle: {
+            color: 'gray.800',
+            _hover: { bg: `inherit`, color: highlightColor },
+            _active: { color: `inherit`, bg: borderColor },
           },
-        },
-        Slider: {
-          baseStyle: () => ({
-            thumb: {
-              backgroundColor: highlightColor,
-            },
-            filledTrack: {
-              backgroundColor: 'gray.400',
-            },
-            track: {
-              backgroundColor: 'gray.400',
-              borderColor: 'gray.400',
-              borderWidth: '5px',
-              borderRadius: 'lg',
-            },
-          }),
         },
       },
-    };
+      Accordion: {
+        baseStyle: {
+          container: {
+            marginTop: '10px',
+            borderWidth: '0px',
+            _last: {
+              borderWidth: '0px',
+            },
+          },
+          panel: {
+            marginRight: '10px',
+          },
+        },
+      },
+      Slider: {
+        baseStyle: () => ({
+          thumb: {
+            backgroundColor: highlightColor,
+          },
+          filledTrack: {
+            backgroundColor: 'gray.400',
+          },
+          track: {
+            backgroundColor: 'gray.400',
+            borderColor: 'gray.400',
+            borderWidth: '5px',
+            borderRadius: 'lg',
+          },
+        }),
+      },
+    },
+  };
+};
+
+function App({ Component, pageProps }: AppProps) {
+  const [emacsTheme, setEmacsTheme] = useState<Theme>(initialTheme);
+  const [highlightColor, setHighlightColor] = useState(initialHighlightColor);
+
+  const theme = useMemo(() => {
+    return constructTheme(highlightColor, emacsTheme);
   }, [highlightColor, JSON.stringify(emacsTheme)]);
 
   const extendedTheme = extendTheme(
     theme,
     withDefaultColorScheme({ colorScheme: highlightColor.split('.')[0] })
   );
-  return <ChakraProvider theme={extendedTheme}>{children}</ChakraProvider>;
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        emacsTheme: emacsTheme,
+        setEmacsTheme: setEmacsTheme,
+        highlightColor: highlightColor,
+        setHighlightColor: setHighlightColor,
+      }}
+    >
+      <ChakraProvider theme={extendedTheme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ThemeContext.Provider>
+  );
 }
-export default MyApp;
+
+export default App;
--- util/themecontext.tsx
+++ util/themecontext.tsx
@@ -1,19 +1,23 @@
 import { createContext } from 'react';
 
 import { themes } from '../components/themes';
-const initialTheme = ['vibrant', themes['one-vibrant']];
 
-export interface ThemeContextProps {
-  emacsTheme: typeof initialTheme;
-  setEmacsTheme: any;
+type ThemeName = string;
+export type ThemeMap = { [color: string]: string };
+export type Theme = [ThemeName, ThemeMap];
+export type ThemeContextProps = {
+  emacsTheme: Theme;
+  setEmacsTheme: (theme: Theme) => void;
   highlightColor: string;
-  setHighlightColor: any;
-}
+  setHighlightColor: (color: string) => void;
+};
 
-const ThemeContext = createContext<ThemeContextProps>({
+export const initialTheme: Theme = ['one-vibrant', themes['one-vibrant']];
+export const initialHighlightColor = 'purple.500';
+
+export const ThemeContext = createContext<ThemeContextProps>({
   emacsTheme: initialTheme,
-  setEmacsTheme: null,
-  highlightColor: 'purple',
-  setHighlightColor: null,
+  setEmacsTheme: () => {},
+  highlightColor: initialHighlightColor,
+  setHighlightColor: () => {},
 });
-export { ThemeContext };
