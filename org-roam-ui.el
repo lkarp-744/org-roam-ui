70df225522e8aa8265c4c52b66886993efcf2ab8
lkarp <69141474+lkarp-744@users.noreply.github.com>
2025-04-09 08:27:35 +0200

    Change mime type

--- org-roam-ui.el
+++ org-roam-ui.el
@@ -311,7 +311,7 @@ TODO: Be able to delete individual nodes."
   (setq org-roam-directory org-roam-ui--roam-directory)
   (progn
     (httpd-send-file t (org-link-decode file))
-    (httpd-send-header t "text/plain" 200 :Access-Control-Allow-Origin "*")))
+    (httpd-send-header t "image/*" 200 :Access-Control-Allow-Origin "*")))
 
 (defun org-roam-ui--on-save ()
   "Send graphdata on saving an org-roam buffer.
